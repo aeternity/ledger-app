afcc670b63ad50a5f285f544842e8bf3d625ffd5
Gaith Hallak <gaithhallak@gmail.com>
2025-02-12 13:48:52 +0400

    Remove useless screens from main menu

--- src/app_ui/menu.rs
+++ src/app_ui/menu.rs
@@ -20,12 +20,10 @@ use ledger_device_sdk::io::Comm;
 
 #[cfg(not(any(target_os = "stax", target_os = "flex")))]
 use ledger_device_sdk::ui::{
-    bitmaps::{Glyph, BACK, CERTIFICATE, DASHBOARD_X},
+    bitmaps::{Glyph, DASHBOARD_X},
     gadgets::{EventOrPageIndex, MultiPageMenu, Page},
 };
 
-#[cfg(any(target_os = "stax", target_os = "flex"))]
-use crate::settings::Settings;
 #[cfg(any(target_os = "stax", target_os = "flex"))]
 use ledger_device_sdk::nbgl::{NbglGlyph, NbglHomeAndSettings};
 
@@ -34,39 +32,20 @@ use crate::Instruction;
 #[cfg(not(any(target_os = "stax", target_os = "flex")))]
 use ledger_device_sdk::io::Event;
 
-// use ledger_device_sdk::nvm::*;
-
-#[cfg(not(any(target_os = "stax", target_os = "flex")))]
-fn ui_about_menu(comm: &mut Comm) -> Event<Instruction> {
-    let pages = [
-        &Page::from((["Rust Boilerplate", "(c) 2023 Ledger"], true)),
-        &Page::from(("Back", &BACK)),
-    ];
-    loop {
-        match MultiPageMenu::new(comm, &pages).show() {
-            EventOrPageIndex::Event(e) => return e,
-            EventOrPageIndex::Index(1) => return ui_menu_main(comm),
-            EventOrPageIndex::Index(_) => (),
-        }
-    }
-}
-
 #[cfg(not(any(target_os = "stax", target_os = "flex")))]
 pub fn ui_menu_main(comm: &mut Comm) -> Event<Instruction> {
     const APP_ICON: Glyph = Glyph::from_include(include_gif!("icons/ae.gif"));
     let pages = [
         // The from trait allows to create different styles of pages
         // without having to use the new() function.
-        &Page::from((["Boilerplate", "is ready"], &APP_ICON)),
+        &Page::from((["Application", "is ready"], &APP_ICON)),
         &Page::from((["Version", env!("CARGO_PKG_VERSION")], true)),
-        &Page::from(("About", &CERTIFICATE)),
         &Page::from(("Quit", &DASHBOARD_X)),
     ];
     loop {
         match MultiPageMenu::new(comm, &pages).show() {
             EventOrPageIndex::Event(e) => return e,
-            EventOrPageIndex::Index(2) => return ui_about_menu(comm),
-            EventOrPageIndex::Index(3) => ledger_device_sdk::exit_app(0),
+            EventOrPageIndex::Index(2) => ledger_device_sdk::exit_app(0),
             EventOrPageIndex::Index(_) => (),
         }
     }
@@ -77,15 +56,11 @@ pub fn ui_menu_main(_: &mut Comm) -> NbglHomeAndSettings {
     // Load glyph from 64x64 4bpp gif file with include_gif macro. Creates an NBGL compatible glyph.
     const FERRIS: NbglGlyph = NbglGlyph::from_include(include_gif!("icons/ae_64.gif", NBGL));
 
-    let settings_strings = [["Display Memo", "Allow display of transaction memo."]];
-    let mut settings: Settings = Default::default();
-
     // Display the home screen.
     NbglHomeAndSettings::new()
         .glyph(&FERRIS)
-        .settings(settings.get_mut(), &settings_strings)
         .infos(
-            "Boilerplate",
+            "Aeternity",
             env!("CARGO_PKG_VERSION"),
             env!("CARGO_PKG_AUTHORS"),
         )
