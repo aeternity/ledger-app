1386804889791524f61d08098db964e589afbaa3
Gaith Hallak <gaithhallak@gmail.com>
2025-02-12 18:43:43 +0400

    Update command sender

--- tests/application_client/command_sender.py
+++ tests/application_client/command_sender.py
@@ -11,83 +11,71 @@ MAX_APDU_LEN: int = 255
 CLA: int = 0xE0
 
 class P1(IntEnum):
-    # Parameter 1 for first APDU number.
     P1_START = 0x00
-    # Parameter 1 for maximum APDU number.
-    P1_MAX   = 0x03
-    # Parameter 1 for screen confirmation for GET_PUBLIC_KEY.
-    P1_CONFIRM = 0x01
+    P1_MORE  = 0x80
+    P1_CONFIRM_NOT_NEEDED = 0x00
+    P1_CONFIRM_NEEDED = 0x01
 
-class P2(IntEnum):
-    # Parameter 2 for last APDU to receive.
-    P2_LAST = 0x00
-    # Parameter 2 for more APDU to receive.
-    P2_MORE = 0x80
+P2: int = 0x00
 
 class InsType(IntEnum):
-    GET_VERSION    = 0x03
-    GET_APP_NAME   = 0x04
-    GET_PUBLIC_KEY = 0x05
-    SIGN_TX        = 0x06
+    GET_ADDRESS = 0x02
+    SIGN_TX     = 0x04
+    GET_VERSION = 0x06
+    SIGN_MSG    = 0x08
 
 class Errors(IntEnum):
-    SW_DENY                    = 0x6985
-    SW_WRONG_P1P2              = 0x6A86
-    SW_INS_NOT_SUPPORTED       = 0x6D00
-    SW_CLA_NOT_SUPPORTED       = 0x6E00
-    SW_WRONG_APDU_LENGTH       = 0x6E03
-    SW_WRONG_RESPONSE_LENGTH   = 0xB000
-    SW_DISPLAY_BIP32_PATH_FAIL = 0xB001
-    SW_DISPLAY_ADDRESS_FAIL    = 0xB002
-    SW_DISPLAY_AMOUNT_FAIL     = 0xB003
-    SW_WRONG_TX_LENGTH         = 0xB004
-    SW_TX_PARSING_FAIL         = 0xB005
-    SW_TX_HASH_FAIL            = 0xB006
-    SW_BAD_STATE               = 0xB007
-    SW_SIGNATURE_FAIL          = 0xB008
+    SW_DENY                     = 0x6985
+    SW_WRONG_P1P2               = 0x6A86
+    SW_INS_NOT_SUPPORTED        = 0x6D00
+    SW_CLA_NOT_SUPPORTED        = 0x6E00
+    SW_WRONG_APDU_LENGTH        = 0x6E03
+    SW_WRONG_RESPONSE_LENGTH    = 0xB000
+    SW_DISPLAY_BIP32_PATH_FAIL  = 0xB001
+    SW_DISPLAY_ADDRESS_FAIL     = 0xB002
+    SW_DISPLAY_AMOUNT_FAIL      = 0xB003
+    SW_WRONG_TX_LENGTH          = 0xB004
+    SW_TX_PARSING_FAIL          = 0xB005
+    SW_TX_HASH_FAIL             = 0xB006
+    SW_BAD_STATE                = 0xB007
+    SW_SIGNATURE_FAIL           = 0xB008
+    SW_MSG_WRONG_LENGTH         = 0xB100,
+    SW_MSG_HASH_FAIL            = 0xB101,
+    SW_MSG_SIGN_FAIL            = 0xB102,
+    SW_GET_ADDRESS_PARSING_FAIL = 0xB200,
 
 
 def split_message(message: bytes, max_size: int) -> List[bytes]:
     return [message[x:x + max_size] for x in range(0, len(message), max_size)]
 
 
-class BoilerplateCommandSender:
+class CommandSender:
     def __init__(self, backend: BackendInterface) -> None:
         self.backend = backend
 
 
-    def get_app_and_version(self) -> RAPDU:
-        return self.backend.exchange(cla=0xB0,  # specific CLA for BOLOS
-                                     ins=0x01,  # specific INS for get_app_and_version
-                                     p1=P1.P1_START,
-                                     p2=P2.P2_LAST,
-                                     data=b"")
-
-
     def get_version(self) -> RAPDU:
         return self.backend.exchange(cla=CLA,
-                                     ins=InsType.GET_VERSION,
-                                     p1=P1.P1_START,
-                                     p2=P2.P2_LAST,
-                                     data=b"")
+                                     ins=InsType.GET_VERSION)
 
 
-    def get_app_name(self) -> RAPDU:
+    def get_address(self, account_number: bytes) -> RAPDU:
         return self.backend.exchange(cla=CLA,
-                                     ins=InsType.GET_APP_NAME,
-                                     p1=P1.P1_START,
-                                     p2=P2.P2_LAST,
-                                     data=b"")
+                                     ins=InsType.GET_ADDRESS,
+                                     p1=P1.P1_CONFIRM_NOT_NEEDED,
+                                     p2=P2,
+                                     data=account_number)
 
 
-    def get_public_key(self, path: str) -> RAPDU:
+    def get_address_with_confirmation(self, account_number: bytes) -> RAPDU:
         return self.backend.exchange(cla=CLA,
-                                     ins=InsType.GET_PUBLIC_KEY,
-                                     p1=P1.P1_START,
-                                     p2=P2.P2_LAST,
-                                     data=pack_derivation_path(path))
+                                     ins=InsType.GET_ADDRESS,
+                                     p1=P1.P1_CONFIRM_NEEDED,
+                                     p2=P2,
+                                     data=account_number)
 
 
+    """
     @contextmanager
     def get_public_key_with_confirmation(self, path: str) -> Generator[None, None, None]:
         with self.backend.exchange_async(cla=CLA,
@@ -125,3 +113,4 @@ class BoilerplateCommandSender:
 
     def get_async_response(self) -> Optional[RAPDU]:
         return self.backend.last_async_response
+    """
