06585a2e2a8b7abd6faa1ee15fe8e62db265faa3
Gaith Hallak <gaithhallak@gmail.com>
2025-02-12 18:01:59 +0400

    Fix test_app_mainmenu for all devices

--- tests/test_app_mainmenu.py
+++ tests/test_app_mainmenu.py
@@ -10,12 +10,10 @@ def test_app_mainmenu(firmware, navigator, test_name):
         instructions = [
             NavInsID.RIGHT_CLICK,
             NavInsID.RIGHT_CLICK,
-            NavInsID.RIGHT_CLICK
         ]
     else:
         instructions = [
             NavInsID.USE_CASE_HOME_SETTINGS,
-            NavInsID.USE_CASE_SUB_SETTINGS_NEXT,
             NavInsID.USE_CASE_SUB_SETTINGS_EXIT
         ]
     navigator.navigate_and_compare(ROOT_SCREENSHOT_PATH, test_name, instructions,
