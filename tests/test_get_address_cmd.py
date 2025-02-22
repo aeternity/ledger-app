98e3ae0175c1e0393eae98f4fcf789664f083290
Gaith Hallak <gaithhallak@gmail.com>
2025-02-22 14:04:13 +0300

    Tests for get_address confirmation and rejection

--- tests/test_get_address_cmd.py
+++ tests/test_get_address_cmd.py
@@ -25,32 +25,33 @@ def test_get_address_no_confirm(backend):
         assert address.decode('ascii') == ref_address.decode('ascii')
 
 
-"""
-# In this test we check that the GET_PUBLIC_KEY works in confirmation mode
-def test_get_public_key_confirm_accepted(backend, scenario_navigator):
+# In this test we check that the GET_ADDRESS works in confirmation mode
+def test_get_address_confirm_accepted(backend, scenario_navigator):
     client = CommandSender(backend)
-    path = "m/44'/1'/0'/0/0"
-    
-    with client.get_address_with_confirmation(account_number=path):
+    account_number = 20
+
+    with client.get_address_with_confirmation(account_number=account_number.to_bytes(4, 'big')):
         scenario_navigator.address_review_approve()
-        
+
     response = client.get_async_response().data
-    _, public_key, _, _ = unpack_get_public_key_response(response)
+    _, address = unpack_get_address_response(response)
 
-    ref_public_key, _ = calculate_public_key_and_chaincode(CurveChoice.Secp256k1, path=path)
-    assert public_key.hex() == ref_public_key
+    path = create_ae_curve_path(account_number)
+    public_key, _ = calculate_public_key_and_chaincode(CurveChoice.Ed25519Slip, path=path)
+    ref_address = b'ak_' + b58encode_check(bytes.fromhex(public_key[2:]))
+
+    assert address.decode('ascii') == ref_address.decode('ascii')
 
 
-# In this test we check that the GET_PUBLIC_KEY in confirmation mode replies an error if the user refuses
-def test_get_public_key_confirm_refused(backend, scenario_navigator):
+# In this test we check that the GET_ADDRESS in confirmation mode replies an error if the user refuses
+def test_get_address_confirm_refused(backend, scenario_navigator):
     client = CommandSender(backend)
-    path = "m/44'/1'/0'/0/0"
+    account_number = 20
 
     with pytest.raises(ExceptionRAPDU) as e:
-        with client.get_address_with_confirmation(account_number=path):
+        with client.get_address_with_confirmation(account_number=account_number.to_bytes(4, 'big')):
             scenario_navigator.address_review_reject()
 
     # Assert that we have received a refusal
     assert e.value.status == Errors.SW_DENY
     assert len(e.value.data) == 0
-"""
