aa647f59248390c43fa549cbe7817d6792ded3a5
Gaith Hallak <gaithhallak@gmail.com>
2025-02-25 13:47:14 +0300

    Format all python files

--- tests/test_version_cmd.py
+++ tests/test_version_cmd.py
@@ -2,12 +2,12 @@ import tomli
 from application_client.command_sender import CommandSender
 from application_client.response_unpacker import unpack_get_version_response
 
+
 # In this test we check the behavior of the device when asked to provide the app version
 def test_version(backend):
-    
     with open("Cargo.toml", "rb") as f:
         data = tomli.load(f)
-    version = tuple(map(int, data['package']['version'].split('.')))
+    version = tuple(map(int, data["package"]["version"].split(".")))
     # Use the app interface instead of raw interface
     client = CommandSender(backend)
     # Send the GET_VERSION instruction
