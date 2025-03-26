cbb5eb02c8732c8460ab0d490b54a02573aaf3ad
Gaith Hallak <gaithhallak@gmail.com>
2025-03-26 21:29:27 +0300

    Fix UI in signing files

--- src/app_ui/sign_msg.rs
+++ src/app_ui/sign_msg.rs
@@ -1,12 +1,4 @@
-#[cfg(not(any(target_os = "stax", target_os = "flex")))]
-use ledger_device_sdk::ui::{
-    bitmaps::{CROSSMARK, CERTIFICATE, VALIDATE_14},
-    gadgets::{Field, MultiFieldReview},
-};
-
-#[cfg(any(target_os = "stax", target_os = "flex"))]
 use include_gif::include_gif;
-#[cfg(any(target_os = "stax", target_os = "flex"))]
 use ledger_device_sdk::nbgl::{Field, NbglGlyph, NbglReview};
 
 use crate::AppSW;
@@ -21,34 +13,16 @@ pub fn ui_display_msg(message_bytes: &[u8]) -> Result<bool, AppSW> {
         },
     ];
 
-    #[cfg(not(any(target_os = "stax", target_os = "flex")))]
-    {
-        let my_review = MultiFieldReview::new(
-            &my_fields,
-            &["Sign", "message"],
-            Some(&CERTIFICATE),
-            "Sign",
-            Some(&VALIDATE_14),
-            "Cancel",
-            Some(&CROSSMARK),
-        );
+    const FERRIS: NbglGlyph = NbglGlyph::from_include(include_gif!("icons/ae_64.gif", NBGL));
+    // Create NBGL review. Maximum number of fields and string buffer length can be customised
+    // with constant generic parameters of NbglReview. Default values are 32 and 1024 respectively.
+    let review: NbglReview = NbglReview::new()
+        .titles(
+            "Review message",
+            "",
+            "Sign message",
+        )
+        .glyph(&FERRIS);
 
-        Ok(my_review.show())
-    }
-
-    #[cfg(any(target_os = "stax", target_os = "flex"))]
-    {
-        const FERRIS: NbglGlyph = NbglGlyph::from_include(include_gif!("icons/ae_64.gif", NBGL));
-        // Create NBGL review. Maximum number of fields and string buffer length can be customised
-        // with constant generic parameters of NbglReview. Default values are 32 and 1024 respectively.
-        let review: NbglReview = NbglReview::new()
-            .titles(
-                "Review message",
-                "",
-                "Sign message",
-            )
-            .glyph(&FERRIS);
-
-        Ok(review.show(&my_fields))
-    }
+    Ok(review.show(&my_fields))
 }
