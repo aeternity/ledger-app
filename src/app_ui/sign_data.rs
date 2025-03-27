892ae5b23d3f3fa35e6a506d9dfff83afd672c4b
Gaith Hallak <gaithhallak@gmail.com>
2025-03-27 22:20:55 +0300

    Use nbgl in the sign_data ui

--- src/app_ui/sign_data.rs
+++ src/app_ui/sign_data.rs
@@ -1,14 +1,6 @@
 use alloc::{borrow::ToOwned, vec::Vec, string::String};
 
-#[cfg(not(any(target_os = "stax", target_os = "flex")))]
-use ledger_device_sdk::ui::{
-    bitmaps::{CERTIFICATE, CROSSMARK, VALIDATE_14},
-    gadgets::{Field, MultiFieldReview},
-};
-
-#[cfg(any(target_os = "stax", target_os = "flex"))]
 use include_gif::include_gif;
-#[cfg(any(target_os = "stax", target_os = "flex"))]
 use ledger_device_sdk::nbgl::{Field, NbglGlyph, NbglReview};
 
 use base64::prelude::{Engine, BASE64_STANDARD};
@@ -26,32 +18,17 @@ pub fn ui_display_data(data_bytes: &[u8]) -> Result<bool, AppSW> {
         None => Vec::new(),
     };
 
-    #[cfg(not(any(target_os = "stax", target_os = "flex")))]
-    {
-        let my_review = MultiFieldReview::new(
-            &my_fields,
-            &["Sign", "data"],
-            Some(&CERTIFICATE),
-            "Sign",
-            Some(&VALIDATE_14),
-            "Cancel",
-            Some(&CROSSMARK),
-        );
-
-        Ok(my_review.show())
-    }
-
     #[cfg(any(target_os = "stax", target_os = "flex"))]
-    {
-        const FERRIS: NbglGlyph = NbglGlyph::from_include(include_gif!("icons/ae_64.gif", NBGL));
-        // Create NBGL review. Maximum number of fields and string buffer length can be customised
-        // with constant generic parameters of NbglReview. Default values are 32 and 1024 respectively.
-        let review: NbglReview = NbglReview::new()
-            .titles("Review data", "", "Sign data")
-            .glyph(&FERRIS);
+    const FERRIS: NbglGlyph = NbglGlyph::from_include(include_gif!("icons/ae_64.gif", NBGL));
+    #[cfg(any(target_os = "nanosplus", target_os = "nanox"))]
+    const FERRIS: NbglGlyph = NbglGlyph::from_include(include_gif!("icons/ae_14.gif", NBGL));
+    // Create NBGL review. Maximum number of fields and string buffer length can be customised
+    // with constant generic parameters of NbglReview. Default values are 32 and 1024 respectively.
+    let review: NbglReview = NbglReview::new()
+        .titles("Review data", "", "Sign data")
+        .glyph(&FERRIS);
 
-        Ok(review.show(&my_fields))
-    }
+    Ok(review.show(&my_fields))
 }
 
 fn format_data(data: &[u8]) -> Option<String> {
